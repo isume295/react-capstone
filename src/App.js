56a4002514b9706f17a4fb436050ba333ae863c1
Sumeya <sumeyaibrahim72@gmail.com>
2023-06-30 07:59:15 +0300

    Setup navigation routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 function App() {
   return (
     <div className="App">
-      home
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
     </div>
   );
 }
